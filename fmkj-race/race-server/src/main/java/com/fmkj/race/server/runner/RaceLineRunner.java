acee148bbc9c35fc85d87bb28c1120b39c5d9f9d
youxun <632017924@qq.com>
2018-10-24 17:45:25 +0800

    任务调度

--- fmkj-race/race-server/src/main/java/com/fmkj/race/server/runner/RaceLineRunner.java
+++ fmkj-race/race-server/src/main/java/com/fmkj/race/server/runner/RaceLineRunner.java
@@ -24,33 +24,37 @@ public class RaceLineRunner implements ApplicationRunner {
     private Long sleepTime = 0L;
 
     @Override
-    public void run(ApplicationArguments args) throws Exception {
+    public void run(ApplicationArguments args){
         while (true){
-            LOGGER.info("程序延迟执行" + sleepTime + "毫秒");
-            List<JoinActivityDto> recordList =  joinactivityrecordService.queryJoinActivityList();
-            if(StringUtils.isEmpty(recordList)){
-                sleepTime += 60000L;
-                Thread.sleep(sleepTime);
-                if(sleepTime > 600000L){//如果大于10分钟
-                    sleepTime = 60000L;
+            try {
+                LOGGER.info("程序延迟执行" + sleepTime + "毫秒");
+                List<JoinActivityDto> recordList =  joinactivityrecordService.queryJoinActivityList();
+                if(StringUtils.isEmpty(recordList)){
+                    sleepTime += 60000L;
+                    Thread.sleep(sleepTime);
+                    if(sleepTime > 600000L){//如果大于10分钟
+                        sleepTime = 60000L;
+                    }
+                }else{
+                    sleepTime = 1L;
                 }
-            }else{
-                sleepTime = 1L;
-            }
 
-            LOGGER.info("执行上链计划开始，一共执行：" + recordList.size() + "条上链计划=================");
-            for(JoinActivityDto dto : recordList){
-                synchronized (this){
-                    boolean result = joinactivityrecordService.onChain(dto);
-                    if (result){
-                        LOGGER.info("恭喜==用户【" + dto.getNickname() +"】上链成功!");
-                    }else{
-                        LOGGER.info("很抱歉==用户【" + dto.getNickname() +"】上链失败, 请检查执行日志!");
-                        break;
+                LOGGER.info("执行上链计划开始，一共执行：" + recordList.size() + "条上链计划=================");
+                for(JoinActivityDto dto : recordList){
+                    synchronized (this){
+                        boolean result = joinactivityrecordService.onChain(dto);
+                        if (result){
+                            LOGGER.info("恭喜==用户【" + dto.getNickname() +"】上链成功!");
+                        }else{
+                            LOGGER.info("很抱歉==用户【" + dto.getNickname() +"】上链失败, 请检查执行日志!");
+                            break;
+                        }
                     }
                 }
+                LOGGER.info("执行上链计划执行结束==================================结束");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-            LOGGER.info("执行上链计划执行结束==================================结束");
 
         }
     }
