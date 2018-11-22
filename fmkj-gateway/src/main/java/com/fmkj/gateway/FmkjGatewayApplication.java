02c6ed52cd3715caee12fd6852b5b93b6b0355af
youxun <632017924@qq.com>
2018-11-22 17:57:12 +0800

    上传文件限制大小

--- fmkj-gateway/src/main/java/com/fmkj/gateway/FmkjGatewayApplication.java
+++ fmkj-gateway/src/main/java/com/fmkj/gateway/FmkjGatewayApplication.java
@@ -4,9 +4,13 @@ import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication(exclude ={DataSourceAutoConfiguration.class, DruidDataSourceAutoConfigure.class})
 @EnableDiscoveryClient
@@ -16,6 +20,20 @@ public class FmkjGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FmkjGatewayApplication.class, args);
+	}
+
+	/**
+	 * 文件上传配置
+	 * @return
+	 */
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		//文件最大
+		factory.setMaxFileSize("10240KB"); //KB,MB
+		/// 设置总上传数据总大小
+		factory.setMaxRequestSize("102400KB");
+		return factory.createMultipartConfig();
 	}
 
 }
--- fmkj-user/user-server/src/main/java/com/fmkj/user/server/UserServerApplication.java
+++ fmkj-user/user-server/src/main/java/com/fmkj/user/server/UserServerApplication.java
@@ -2,10 +2,14 @@ package com.fmkj.user.server;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.servlet.MultipartConfigElement;
 
 @SpringCloudApplication
 @EnableFeignClients
@@ -16,5 +20,19 @@ public class UserServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserServerApplication.class, args);
+    }
+
+    /**
+     * 文件上传配置
+     * @return
+     */
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //文件最大
+        factory.setMaxFileSize("10240KB"); //KB,MB
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize("102400KB");
+        return factory.createMultipartConfig();
     }
 }
